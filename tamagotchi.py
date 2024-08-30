be191c19a26682b2f318ddc781c8de3f3efac17a
Hellozsoza <100070022+Hellozsoza@users.noreply.github.com>
2024-08-30 20:49:25 +0200

    Add files via upload

--- tamagotchi.py
+++ tamagotchi.py
@@ -29,9 +29,13 @@ window.geometry("700x500")
 window.configure(bg = "#753737")
 
 running = True
+running1 = True
 closed = False
+thirstExited = False
 hunger = 0
+thirst = 0
 text1 = f"Hunger: {hunger}/10"
+text2 = f"Thirst: {thirst}/10"
 
 def closeApp():
     global closed
@@ -39,42 +43,85 @@ def closeApp():
 
 window.protocol("WM_DELETE_WINDOW", closeApp)
 
+def stopThirst():
+    global running1
+    running1 = False
+
 def hungerHigher():
-    global hunger, text1, running, closed
+    global hunger, text1, running, closed, thirstExited, running1, thirst
     while running:
-        time.sleep(4.5)
-        if hunger < 10 and not hunger < 0 and not closed:
+        if hunger < 10 and not hunger < 0 and not thirst < 0 and not closed:
+            time.sleep(4.5)
             hunger += 1
             text1 = f"Hunger: {hunger}/10"
         elif not closed:
             text1 = "Your pet sadly died. 🪦"
-            canvas.itemconfig(writtenText, text=text1)
+            canvas.delete(thirstText)
+            canvas.itemconfig(hungerText, text=text1)
             canvas.delete(image)
             canvas.itemconfig(yourPetText, text="You don't have a living pet")
             time.sleep(5)
             break
         else:
             text1 = "You killed your pet?! 🗡️"
-            canvas.itemconfig(writtenText, text=text1)
+            canvas.delete(thirstText)
+            canvas.itemconfig(hungerText, text=text1)
             canvas.delete(image)
             canvas.itemconfig(yourPetText, text="You don't have a living pet")
             time.sleep(1.25)
             break
-        canvas.itemconfig(writtenText, text=text1)
+        canvas.itemconfig(hungerText, text=text1)
+    stopThirst()
+    while True:
+        if not thirstExited:
+            continue
+        else:
+            break
     window.quit()
 
 def hungerLower():
-    global hunger, text1
+    global hunger, text1, thirst
     if running and not closed:
-        if hunger > 0:
+        if hunger > 0 and not thirst < 0:
             hunger -= 1
             text1 = f"Hunger: {hunger}/10"
         else:
             hunger -= 2
-    canvas.itemconfig(writtenText, text=text1)
+    canvas.itemconfig(hungerText, text=text1)
+
+def thirstHigher():
+    global thirst, text1, text2, running, closed, thirstExited, running1
+    while running1:
+        if thirst < 10 and not thirst < 0 and not closed:
+            time.sleep(4.5)
+            thirst += 1
+            text2 = f"Thirst: {thirst}/10"
+            canvas.itemconfig(thirstText, text=text2)
+        elif not closed:
+            text1 = "Your pet sadly died. 🪦"
+            canvas.delete(thirstText)
+            canvas.itemconfig(hungerText, text=text1)
+            canvas.delete(image)
+            canvas.itemconfig(yourPetText, text="You don't have a living pet")
+            time.sleep(5)
+            break
+        canvas.itemconfig(hungerText, text=text1)
+    thirstExited = True
+
+def thirstLower():
+    global thirst, text2
+    if running and not closed:
+        if thirst > 0:
+            thirst -= 1
+            text2 = f"Thirst: {thirst}/10"
+        else:
+            thirst -= 2
+    canvas.itemconfig(thirstText, text=text2)
 
 def start():
     thread = threading.Thread(target=hungerHigher)
+    thread1 = threading.Thread(target=thirstHigher)
+    thread1.start()
     thread.start()
 
 canvas = Canvas(
@@ -113,11 +160,36 @@ button.place(
     height=50.0
 )
 
-writtenText = canvas.create_text(
+button_image1 = PhotoImage(
+    file=relative_to_assets("button1.png"))
+button1 = Button(
+    image=button_image1,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: thirstLower(),
+    relief="flat"
+)
+button1.place(
+    x=454.9999999999999,
+    y=280.0,
+    width=200.0,
+    height=50.0
+)
+
+hungerText = canvas.create_text(
     289.9999999999999,
     50.0,
     anchor="nw",
     text=text1,
+    fill="#FFFFFF",
+    font=("Inter", 32 * -1)
+)
+
+thirstText = canvas.create_text(
+    289.9999999999999,
+    125.0,
+    anchor="nw",
+    text=text2,
     fill="#FFFFFF",
     font=("Inter", 32 * -1)
 )
